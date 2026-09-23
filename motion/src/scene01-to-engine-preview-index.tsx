1eb7bed9d522cf79441df7e03104036f44c151ab
IsmaelGeraldo <adming.kais@gmail.com>
2026-09-23 19:41:18 -0300

    Use polished intelligence presentation in preview

--- motion/src/scene01-to-engine-preview-index.tsx
+++ motion/src/scene01-to-engine-preview-index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import {Composition, registerRoot} from 'remotion';
-import {Scene01ToIntelligenceFlow} from './Scene01ToIntelligenceFlow';
+import {Scene01ToIntelligencePolish} from './Scene01ToIntelligencePolish';
 
 const Scene01ToEnginePreviewRoot: React.FC = () => {
   return (
     <Composition
       id="GKAISScene01ToEnginePreview"
-      component={Scene01ToIntelligenceFlow}
+      component={Scene01ToIntelligencePolish}
       durationInFrames={510}
       fps={30}
       width={1280}

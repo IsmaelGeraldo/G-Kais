5b86bd1ba7fd7679ef95bab09dd85c1b3b69d662
IsmaelGeraldo <adming.kais@gmail.com>
2026-09-23 18:11:12 -0300

    Extend preview through intelligence windows

--- motion/src/scene01-to-engine-preview-index.tsx
+++ motion/src/scene01-to-engine-preview-index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import {Composition, registerRoot} from 'remotion';
-import {Scene01ToEngineFlow} from './Scene01ToEngineFlow';
+import {Scene01ToIntelligenceFlow} from './Scene01ToIntelligenceFlow';
 
 const Scene01ToEnginePreviewRoot: React.FC = () => {
   return (
     <Composition
       id="GKAISScene01ToEnginePreview"
-      component={Scene01ToEngineFlow}
-      durationInFrames={288}
+      component={Scene01ToIntelligenceFlow}
+      durationInFrames={420}
       fps={30}
       width={1280}
       height={720}

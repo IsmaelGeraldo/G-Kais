4ac49c4baf50937e7b8682c2c813fb7e08b1e8e4
IsmaelGeraldo <adming.kais@gmail.com>
2026-09-23 22:18:55 -0300

    Use single structural polish layer

--- motion/src/GKaisFinalMaster.tsx
+++ motion/src/GKaisFinalMaster.tsx
@@ -2,16 +2,14 @@ import React from 'react';
 import {AbsoluteFill} from 'remotion';
 import {Scene01ToCRMPreview} from './Scene01ToCRMPreview';
 import {Scene01MasterPolish} from './Scene01MasterPolish';
-import {FinalMasterPolish} from './FinalMasterPolish';
-import {FinalContinuityFix} from './FinalContinuityFix';
+import {FinalStructuralPolish} from './FinalStructuralPolish';
 
 export const GKaisFinalMaster: React.FC = () => {
   return (
     <AbsoluteFill style={{background: '#F6F7F5', overflow: 'hidden'}}>
       <Scene01ToCRMPreview />
       <Scene01MasterPolish />
-      <FinalMasterPolish />
-      <FinalContinuityFix />
+      <FinalStructuralPolish />
     </AbsoluteFill>
   );
 };

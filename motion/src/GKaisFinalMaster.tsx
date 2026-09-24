c29ab9dfbdb30850e91734f65adebee4a3aa1c4a
IsmaelGeraldo <adming.kais@gmail.com>
2026-09-23 21:08:01 -0300

    Create unified final master composition

---
+++ motion/src/GKaisFinalMaster.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {AbsoluteFill} from 'remotion';
+import {Scene01ToCRMPreview} from './Scene01ToCRMPreview';
+import {Scene01MasterPolish} from './Scene01MasterPolish';
+
+export const GKaisFinalMaster: React.FC = () => {
+  return (
+    <AbsoluteFill style={{background: '#F6F7F5', overflow: 'hidden'}}>
+      <Scene01ToCRMPreview />
+      <Scene01MasterPolish />
+    </AbsoluteFill>
+  );
+};
